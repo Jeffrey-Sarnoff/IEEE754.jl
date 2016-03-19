644fee1e449fc758c3eb2095e80752d268d8a833
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 01:43:16 -0400

    Update IEEE_754_2008.jl

--- src/IEEE_754_2008.jl
+++ src/IEEE_754_2008.jl
@@ -1,4 +1,7 @@
 module IEEE_754_2008
 
+export negate, copySign, 
+    isSignMinus, isNormal, isFinite, is Zero, isSubnormal, isInfinite, isNaN,
+    minNum, maxNum, minNumMag, maxNumMag, totalOrder, totalOrderMag
 
 end # module
