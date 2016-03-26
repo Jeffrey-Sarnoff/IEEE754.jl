f2d1bc12f7e08cc5ddcce8038d7bccc11109d983
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 00:49:14 -0400

    Update ulpufp.jl

--- src/ulpufp.jl
+++ src/ulpufp.jl
@@ -1,11 +1,22 @@
+#=
+ACCURATE FLOATING-POINT SUMMATION PART I: FAITHFUL ROUNDING
+Siegfried M. Rump, Takeshi Ogita, and Shin'ichi Oishi
+in SIAM J. Sci. Comput. Volume 31, Issue 1, pp. 189-224 (2008)
+
+ERROR ESTIMATION OF FLOATING-POINT SUMMATION AND DOT PRODUCT
+Siegfried M. Rump
+in BIT, May 18, 2011
+=#
+
 import Base: precision, ldexp
 
-export epsilon, eta
+export epsilon, eta, ufp
 
 for F in (:Float64, :Float32, :Float16)
    @eval begin
-       epsilon(::Type{$F}) = ldexp(one($F), -precision($F))   # dist from 1 to the next smaller float
-       epsilon(x::($F)) = epsilon($F)*x                       # dist from x to next lesser mag float
-       eta(::Type{$F}) = nextfloat(zero($F))                  # smallest nonzero subnormal
+       epsilon(::Type{$F}) = ldexp(one($F), -precision($F))     # dist from 1 to the next smaller float
+       epsilon(x::($F)) = flipsign(abs(x)-prevfloat(abs(x)),x)) # dist from x to next lesser mag float
+       eta(::Type{$F}) = nextfloat(zero($F))                    # smallest nonzero subnormal
+       ufp(x::($F)) = ldexp(one($F), floor(Int, log2(abs(x))))
    end
 end
