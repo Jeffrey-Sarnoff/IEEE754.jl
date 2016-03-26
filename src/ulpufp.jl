53b6fbf99c25dd77b26b1f1a364fdef93f0abb4e
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 00:32:55 -0400

    Update ulpufp.jl

--- src/ulpufp.jl
+++ src/ulpufp.jl
@@ -1,1 +1,11 @@
+import Base: precision, ldexp
 
+export epsilon, eta
+
+for F in (:Float64, :Float32, :Float16)
+   @eval begin
+       epsilon(::Type{$F}) = ldexp(one($F), -precision($F))   # dist from 1 to the next smaller float
+       epsilon(x::($F)) = epsilon($F)*x                       # dist from x to next lesser mag float
+       eta(::Type{$F}) = nextfloat(zero($F))                  # smallest nonzero subnormal
+   end
+end
