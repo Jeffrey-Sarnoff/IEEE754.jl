ce498a79795699ce16e3a9b0bad8a413b4fdc328
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 00:53:38 -0400

    Update ulpufp.jl

--- src/ulpufp.jl
+++ src/ulpufp.jl
@@ -10,13 +10,16 @@ in BIT, May 18, 2011
 
 import Base: precision, ldexp
 
-export epsilon, eta, ufp
+export epsilon, eta, ufp, ulp, sufp, sulp
 
 for F in (:Float64, :Float32, :Float16)
    @eval begin
        epsilon(::Type{$F}) = ldexp(one($F), -precision($F))     # dist from 1 to the next smaller float
-       epsilon(x::($F)) = flipsign(abs(x)-prevfloat(abs(x)),x)) # dist from x to next lesser mag float
+       epsilon(x::($F)) = flipsign(abs(x)-prevfloat(abs(x)),x)  # dist from x to next lesser mag float
        eta(::Type{$F}) = nextfloat(zero($F))                    # smallest nonzero subnormal
-       ufp(x::($F)) = ldexp(one($F), floor(Int, log2(abs(x))))
+       ufp(x::($F)) = ldexp(one($F), floor(Int, log2(abs(x))))  # unit first place
+       ulp(x::($F)) = ldexp(one($F), floor(Int, log2(abs(x))) - precision($F)) # unit last place
+       sufp(x::($F)) = flipsign(ufp(x),x)                       # signed unit first place
+       sulp(x::($F)) = flipsign(ulp(x),x)                       # signed unit last place
    end
 end
