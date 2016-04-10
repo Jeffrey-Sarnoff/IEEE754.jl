9a8a2c317c1454a2a63c1fad41022d8a2f916364
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-10 02:22:43 -0400

    Update FloatSafeInts.jl

--- src/FloatSafeInts.jl
+++ src/FloatSafeInts.jl
@@ -28,27 +28,27 @@ isfloatsafe{T<:StdFloat}(x::T) = ((x % 1) == zero(T)) & (abs(x) < (safemax(T)) +
 isintsafe{T<:StdInt}(x::T)     = abs(x) < (safemax(T)) + one(T))
 isintsafe{T<:StdFloat}(x::T)   = ((x % 1) == zero(T)) & (abs(x) < (safemax(T)) + one(T))
 
-"""
+@doc"""
 _safemax(FloatType|IntType)_
 
 The largest positive integer with sizeof() matching given type such that it
 and its predecessor interconvert with sizeof matching float exactly.
 """->safemax
 
-"""
+@doc"""
 _safemin(FloatType|IntType)_
 
 The largest negative integer with sizeof() matching given type such that it
 and its predecessor interconvert with sizeof matching float exactly.
 """->safemin
 
-"""
+@doc"""
 _isfloatsafe(FloatValue|IntValue)_
 
 Tests that the abs(value) and its predecessor interconvert to IntValue|FloatValue exactly.
 """->isfloatsafe
 
-"""
+@doc"""
 _isintsafe(IntValue|FloatValue)_
 
 Tests that the abs(value) and its predecessor interconvert to FloatValue|IntValue exactly.
