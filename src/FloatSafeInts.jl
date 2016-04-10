f6035477d223226f9168d429e31744cda419d46d
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-10 02:10:33 -0400

    Create FloatSafeInts.jl

---
+++ src/FloatSafeInts.jl
@@ -0,0 +1,31 @@
+module FloatSafeInts
+
+import Base: trailing_ones, significand_mask
+
+export safemax, safemin, isfloatsafe, isintsafe
+
+safemax(::Type{Float64}) = one(Int64) << (1 + trailing_ones(significand_mask(Float64)))
+safemax(::Type{Float32}) = one(Int32) << (1 + trailing_ones(significand_mask(Float32)))
+safemax(::Type{Float16}) = one(Int16) << (1 + trailing_ones(significand_mask(Float16)))
+
+safemin(::Type{Float64}) = -safemax(Float64)
+safemin(::Type{Float32}) = -safemax(Float32)
+safemin(::Type{Float16}) = -safemax(Float16)
+
+safemax(::Type{Int64}) = safemax(Float64)
+safemax(::Type{Int32}) = safemax(Float32)
+safemax(::Type{Int16}) = safemax(Float16)
+safemin(::Type{Int64}) = safemin(Float64)
+safemin(::Type{Int32}) = safemin(Float32)
+safemin(::Type{Int16}) = safemin(Float16)
+
+typealias StdFloat Union{Float64,Float32,Float16}
+typealias StdInt   Union{Int64,Int32,Int16}
+
+isfloatsafe{T<:StdInt}(x::T)   = abs(x) < (safemax(T)) + one(T))
+isfloatsafe{T<:StdFloat}(x::T) = ((x % 1) == zero(T)) & (abs(x) < (safemax(T)) + one(T))
+
+isintsafe{T<:StdInt}(x::T)     = abs(x) < (safemax(T)) + one(T))
+isintsafe{T<:StdFloat}(x::T)   = ((x % 1) == zero(T)) & (abs(x) < (safemax(T)) + one(T))
+
+end # FloatSafeInts
