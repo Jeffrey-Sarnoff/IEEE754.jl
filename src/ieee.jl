e9f74ed6ad67a0b80686cdb27fe2491c98ded251
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-10 01:40:47 -0400

    Update ieee.jl

--- src/ieee.jl
+++ src/ieee.jl
@@ -1,1 +1,18 @@
+module SignificantBits
 
+import Base: significand_mask, trailing_ones
+
+export significand_bits, # explicit 
+       significant_bits  # implicit = explicit + 1
+
+# how many bits are present in the encoded significand (excludes the hidden bit)
+significand_bits(::Type{Float64}) = trailing_ones(significand_mask(Float64))
+significand_bits(::Type{Float32}) = trailing_ones(significand_mask(Float32))
+significand_bits(::Type{Float16}) = trailing_ones(significand_mask(Float16))
+
+# how many bits are represented with the significand (includes the hidden bit)
+significant_bits(::Type{Float64}) = 1+significand_bits(Float64)
+significant_bits(::Type{Float32}) = 1+significand_bits(Float32)
+significant_bits(::Type{Float16}) = 1+significand_bits(Float16)
+
+end # SignificantBits
