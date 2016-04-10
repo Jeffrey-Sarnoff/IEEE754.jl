e6e11f53fdb7aafdbb0621528ad1365f2b8613a0
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-10 01:42:59 -0400

    Update IEEE_754_2008.jl

--- src/IEEE_754_2008.jl
+++ src/IEEE_754_2008.jl
@@ -2,13 +2,13 @@ module IEEE_754_2008
 
 import Base: min, max, minmax, precision, ldexp, frexp
 
-include("ieee.jl")
 include("extensions.jl")
 include("ulpufp.jl")
 
 # include bypasses precompilation
 # modulename = :QNaN; @eval begin import $modulename; using $modulename end
 modulename = :QNaN; @eval importall $modulename
+modulename = :SignificantBits; @eval importall $modulename
 
 export negate, copySign, 
     isSignMinus, isNormal, isFinite, is Zero, isSubnormal, isInfinite, isNaN,
@@ -23,6 +23,8 @@ export negate, copySign,
     epsilon, eta, ulp, ufp, sulp, sufp,
     # QNaN
     qnan
+    # SignificantBits
+    significand_bits, significant_bits
 
 
 end # module
